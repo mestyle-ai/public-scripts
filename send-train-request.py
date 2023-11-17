e809131bf7b30fba9d800cfe6b396cf2c76cdad4
parnurzeal <parnurzeal@gmail.com>
2023-11-18 03:38:08 +0800

    Adjust send requst to read image

--- send-train-request.py
+++ send-train-request.py
@@ -1,4 +1,12 @@
+import base64
 import requests
+import os
+
+image_path = os.path.expanduser('~') + '/atw/images/atd_bottle/bottle_with_orange.jpg'
+
+with open(image_path, 'rb') as image_file:
+    base64_bytes = base64.b64encode(image_file.read())
+
 
 # The API endpoint
 url = "http://127.0.0.1:8080/dreambooth/v1/train"
@@ -9,14 +17,14 @@ new_data = {
         "images": [
             {
                 "filename": "img1.png",
-                "base64content": ""
+                "base64content": base64_bytes.decode('utf-8'),
                 }
             ],
         "ref_id": 1234,
         }
 
 # A GET request to the API
-response = requests.post(url)
+response = requests.post(url, json=new_data)
 
 # Print the response
 response_json = response.json()
